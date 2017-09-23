0af818a17170201c6441f50e19d7f370ee80ab21
Wesley-4a5a <374053@edu.rocmn.nl>
2017-09-23 21:13:25 +0200

    simpel formke

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,23 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
+  template: `
+    <form (ngSubmit)="logForm()">
+      <ion-item>
+        <ion-label>Todo</ion-label>
+        <ion-input type="text" [(ngModel)]="todo.title" name="title"></ion-input>
+      </ion-item>
+      <ion-item>
+        <ion-label>Description</ion-label>
+        <ion-textarea [(ngModel)]="todo.description" name="description"></ion-textarea>
+      </ion-item>
+      <button ion-button type="submit" block>Add Todo</button>
+    </form>
+  `,
+})
+
+
+@Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
@@ -9,6 +26,11 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  todo = {}
+  logForm() {
+    console.log(this.todo)
   }
 
 }
