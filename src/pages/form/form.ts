2e74c9552db495a43f6eb51e5bb33d5153434860
Wesley-4a5a <374053@edu.rocmn.nl>
2017-09-24 13:11:52 +0200

    pdf creator

--- src/pages/form/form.ts
+++ src/pages/form/form.ts
@@ -15,18 +15,21 @@ export class FormPage {
 	forms: FirebaseListObservable<any>;
 
   constructor(public navCtrl: NavController, af: AngularFireDatabase) {
-	this.forms = af.list('/beoordeelforms');
+	   this.forms = af.list('/beoordeelforms');
   }
 
   todo = {}
   logForm() {
-	this.forms.push(this.todo);
+	   this.forms.push(this.todo);
   }
 
-  public download() {
-console.log('loll');
+  public download(id) {
+        console.log(id.naam);
         let doc = new jsPDF();
-        doc.text(20,20,'Hello world');
+        let beoordeling = id.beoordeling
+        let beoordeling1 = doc.splitTextToSize("Beoordeling:" + beoordeling, 7.5);
+        doc.text(20,20,"Naam:" + id.naam);
+        doc.text(20,30,"Beoordeling:" + beoordeling1);
         doc.save('Test.pdf');
     }
 
