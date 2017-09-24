5409e5d3b559424d98c2e92c979cdeeff557565a
Wesley-4a5a <374053@edu.rocmn.nl>
2017-09-24 12:19:36 +0200

    pdf creator added

--- src/pages/form/form.ts
+++ src/pages/form/form.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+import jsPDF from 'jspdf'
+declare let jsPDF;
 
 @Component({
   selector: 'page-form',
@@ -15,9 +17,17 @@ export class FormPage {
   constructor(public navCtrl: NavController, af: AngularFireDatabase) {
 	this.forms = af.list('/beoordeelforms');
   }
-  
+
   todo = {}
   logForm() {
-	this.forms.push(this.todo);	
+	this.forms.push(this.todo);
   }
+
+  public download() {
+console.log('loll');
+        let doc = new jsPDF();
+        doc.text(20,20,'Hello world');
+        doc.save('Test.pdf');
+    }
+
 }
