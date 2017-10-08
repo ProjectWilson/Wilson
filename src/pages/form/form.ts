de43100abfdd39d83f8de5fb5ac4bd7dbdd2428e
Ferdi <323957@edu.rocmn.nl>
2017-10-08 23:18:41 +0200

    no message

--- src/pages/form/form.ts
+++ src/pages/form/form.ts
@@ -19,114 +19,47 @@ export class FormPage {
 	   this.forms = af.list('/gesprekforms');
   }
 
-  todo = {}
+  gesprek = {}
     logForm(id) {
-      if(id.datum == null)
-        {
-          let date = new Date;
-          let year = date.getFullYear();
-          let month = date.getMonth() + 1;
-          let day = date.getDate();
-          let datum = day + '-' + month + '-' + year;
-
-          this.todo['datum'] = datum;
-          console.log(1);
-        }
-
-
-      if(id.akkoord == true)
+      if(this.gesprek['datum'] == null)
       {
-          this.forms.push(this.todo);
+        let date = new Date;
+        let year = date.getFullYear();
+        let month = date.getMonth() + 1;
+        let day = date.getDate();
+        let datum = year + '-' + month + '-' + day;
+
+        this.gesprek['datum'] = datum;
       }
-      else{
-        this.presentAlert();
-      }
+      console.log(this.gesprek);
+      // if(id.akkoord == true)
+      // {
+          this.forms.push(this.gesprek);
+      // }
+      // else
+      // {
+      //   this.presentAlert();
+      // }
     }
 
     inputEnable() {
+        // console.log($ionicSlideBoxDelegate);
         document.querySelector('input[name="bpvdocent"]').removeAttribute('disabled');
         document.querySelector('input[name="praktijkopleider"]').removeAttribute('disabled');
     }
-    // test(id) {
-    //     // $scope.editEmail = true;
-    //
-    //     let app = angular.module('myApp', []);
-    //     app.controller('myCtrl', function($scope) {
-    //         $scope.carname = "Volvo";
-    //     });
-    // }
-    //
+
+    presentAlert() {
+        let alert = this.alertCtrl.create({
+            title: 'Akkoord',
+            subTitle: 'Ga akkoord met het formulier',
+            buttons: ['Sluit']
+        });
+        alert.present();
+    }
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  public download(id) {
-          console.log(id.naam);
-
-          let pageWidth = 8.5,
-          lineHeight = 1.2,
-          margin = 0.5,
-          maxLineWidth = pageWidth - margin * 2,
-          fontSize = 14,
-          ptsPerInch = 72,
-          oneLineHeight = fontSize * lineHeight / ptsPerInch,
-          text = 'Naam: \n' + id.naam + '\n\n' +
-              'BPV Docent: \n' + id.bpvdocent + '\n\n' +
-              'BPV Bedrijf: \n' + id.bpvbedrijf + '\n\n' +
-              'Praktijkopleider: \n' + id.praktijkopleider + '\n\n' +
-              'Datum: \n' + id.datum + '\n\n' +
-              'Beoordeling: \n' + id.beoordeling + '\n\n';
-
-          let doc = new jsPDF({
-              unit: 'in',
-              lineHeight: lineHeight
-            }).setProperties({ title: 'Gespreksformulier' });
-
-          let textLines = doc
-              .setFont('helvetica', 'neue')
-              .setFontSize(fontSize)
-              .splitTextToSize(text, maxLineWidth);
-
-          //Create de tekst en pdf format
-          doc.text(textLines, margin, margin + 2 * oneLineHeight);
-
-          let textHeight = textLines.length * fontSize * lineHeight / ptsPerInch;
-              doc
-                  .setFontStyle('bold')
-                  .text('Gespreksformulier:', margin, margin + oneLineHeight);
-
-
-        //maakt rectangles
-          doc.setLineWidth(0);
-          doc.rect(margin, textHeight + 3, 3, oneLineHeight + 1);
-          doc.text('Handtekening Student', margin, textHeight + 2.5);
-		  doc.rect(margin + 4, textHeight + 3, 3, oneLineHeight + 1);
-		  doc.text('Handtekening Begeleider', margin + 4, textHeight + 2.5);
-          doc.save('Test.pdf');
-      }
-
-      presentAlert() {
-      let alert = this.alertCtrl.create({
-        title: 'Akkoord',
-        subTitle: 'Ga akkoord met het formulier',
-        buttons: ['Sluit']
-      });
-      alert.present();
-}
 }
