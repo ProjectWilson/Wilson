f8fd986ea40be0c1017a0f93ff1b4cab6a32e0b6
Koning, W. (Wesley) <374053@edu.rocmn.nl>
2017-10-10 09:50:10 +0200

    cmt
    cmt

--- src/pages/beoordelingDB/beoordelingDB.ts
+++ src/pages/beoordelingDB/beoordelingDB.ts
@@ -42,48 +42,48 @@ export class BeoordelingDBPage {
       }
 
   public download(id) {
-          console.log(id.naam);
-
-          let pageWidth = 8.5,
-          lineHeight = 1.2,
-          margin = 0.5,
-          maxLineWidth = pageWidth - margin * 2,
-          fontSize = 14,
-          ptsPerInch = 72,
-          oneLineHeight = fontSize * lineHeight / ptsPerInch,
-          text = 'Naam: \n' + id.naam + '\n\n' +
-              'BPV Docent: \n' + id.bpvdocent + '\n\n' +
-              'BPV Bedrijf: \n' + id.bpvbedrijf + '\n\n' +
-              'Praktijkopleider: \n' + id.praktijkopleider + '\n\n' +
-              'Datum: \n' + id.datum + '\n\n' +
-              'Beoordeling: \n' + id.beoordeling + '\n\n';
-
-          let doc = new jsPDF({
-              unit: 'in',
-              lineHeight: lineHeight
-            }).setProperties({ title: 'Gespreksformulier' });
-
-          let textLines = doc
-              .setFont('helvetica', 'neue')
-              .setFontSize(fontSize)
-              .splitTextToSize(text, maxLineWidth);
-
-          //Create de tekst en pdf format
-          doc.text(textLines, margin, margin + 2 * oneLineHeight);
-
-          let textHeight = textLines.length * fontSize * lineHeight / ptsPerInch;
-              doc
-                  .setFontStyle('bold')
-                  .text('Gespreksformulier:', margin, margin + oneLineHeight);
-
-
-        //maakt rectangles
-          doc.setLineWidth(0);
-          doc.rect(margin, textHeight + 3, 3, oneLineHeight + 1);
-          doc.text('Handtekening Student', margin, textHeight + 2.5);
-		  doc.rect(margin + 4, textHeight + 3, 3, oneLineHeight + 1);
-		  doc.text('Handtekening Begeleider', margin + 4, textHeight + 2.5);
-          doc.save('Test.pdf');
+      //     console.log(id.naam);
+      //
+      //     let pageWidth = 8.5,
+      //     lineHeight = 1.2,
+      //     margin = 0.5,
+      //     maxLineWidth = pageWidth - margin * 2,
+      //     fontSize = 14,
+      //     ptsPerInch = 72,
+      //     oneLineHeight = fontSize * lineHeight / ptsPerInch,
+      //     text = 'Naam: \n' + id.naam + '\n\n' +
+      //         'BPV Docent: \n' + id.bpvdocent + '\n\n' +
+      //         'BPV Bedrijf: \n' + id.bpvbedrijf + '\n\n' +
+      //         'Praktijkopleider: \n' + id.praktijkopleider + '\n\n' +
+      //         'Datum: \n' + id.datum + '\n\n' +
+      //         'Beoordeling: \n' + id.beoordeling + '\n\n';
+      //
+      //     let doc = new jsPDF({
+      //         unit: 'in',
+      //         lineHeight: lineHeight
+      //       }).setProperties({ title: 'Gespreksformulier' });
+      //
+      //     let textLines = doc
+      //         .setFont('helvetica', 'neue')
+      //         .setFontSize(fontSize)
+      //         .splitTextToSize(text, maxLineWidth);
+      //
+      //     //Create de tekst en pdf format
+      //     doc.text(textLines, margin, margin + 2 * oneLineHeight);
+      //
+      //     let textHeight = textLines.length * fontSize * lineHeight / ptsPerInch;
+      //         doc
+      //             .setFontStyle('bold')
+      //             .text('Gespreksformulier:', margin, margin + oneLineHeight);
+      //
+      //
+      //   //maakt rectangles
+      //     doc.setLineWidth(0);
+      //     doc.rect(margin, textHeight + 3, 3, oneLineHeight + 1);
+      //     doc.text('Handtekening Student', margin, textHeight + 2.5);
+		  // doc.rect(margin + 4, textHeight + 3, 3, oneLineHeight + 1);
+		  // doc.text('Handtekening Begeleider', margin + 4, textHeight + 2.5);
+      //     doc.save('Test.pdf');
       }
 
       presentAlert() {
