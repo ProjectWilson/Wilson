5e7f703e5756d5a3e1dd46cfa672543d717eb167
Koning, W. (Wesley) <374053@edu.rocmn.nl>
2017-10-10 09:07:55 +0200

    AutofillFake
    fke

--- src/pages/form/form.ts
+++ src/pages/form/form.ts
@@ -14,15 +14,45 @@ import { Slides } from 'ionic-angular';
 export class FormPage {
 
     forms: FirebaseListObservable<any>;
+    gebruikers: FirebaseListObservable<any>;
     @ViewChild(Slides) slides: Slides;
 
     constructor(public navCtrl: NavController, af: AngularFireDatabase, private alertCtrl: AlertController) {
         this.forms = af.list('/gesprekforms');
+        this.gebruikers = af.list('/gebruikers');
     }
 
     radioValue(value) {
         this.gesprek['BblOrBol'] = value;
     }
+
+    studentenlijst = {
+            'studentnummer': '',
+            'roepnaam': '',
+            'voorvoegsel': '',
+            'achternaam': '',
+            'telefoon': '',
+            'woonplaats': '',
+            'opleiding': '',
+            'email': '',
+            'praktijkopleider': '',
+            'bedrijf': '',
+            'bpvtelefoon': '',
+            'bpvbegeleider': ''
+        };
+
+        StudentnummerInput;
+        autofill(test)
+        {
+            console.log(test);
+            this.gesprek = {
+                'naam' : this.studentenlijst.roepnaam + this.studentenlijst.voorvoegsel + this.studentenlijst.achternaam,
+                'studentnummer' : this.studentenlijst.studentnummer ,
+                'bpvdocent' : this.studentenlijst.bpvbegeleider,
+                'bpvbedrijf' : this.studentenlijst.bedrijf,
+                'praktijkopleider' : this.studentenlijst.praktijkopleider
+            };
+        }
 
     gesprek = {}
     logForm(id) {
