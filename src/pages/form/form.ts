4f6c9e91e03dc0917201302be0cf771b8889d89c
Wesley-4a5a <374053@edu.rocmn.nl>
2017-10-22 13:41:49 +0200

    Improved Augmented

--- src/pages/form/form.ts
+++ src/pages/form/form.ts
@@ -19,33 +19,33 @@ export class FormPage {
        this.gebruikers = af.list('/gebruikers');
   }
 
-	studentenlijst = {
-		'studentnummer': '',
-		'roepnaam': '',
-		'voorvoegsel': '',
-		'achternaam': '',
-		'telefoon': '',
-		'woonplaats': '',
-		'opleiding': '',
-		'email': '',
-		'praktijkopleider': '',
-		'bedrijf': '',
-		'bpvtelefoon': '',
-		'bpvbegeleider': ''
-	};
-
-	StudentnummerInput;
-	autofill(test)
-	{
-		console.log(test);
-		this.gesprek = {
-			'naam' : this.studentenlijst.roepnaam + this.studentenlijst.voorvoegsel + this.studentenlijst.achternaam,
-			'studentnummer' : this.studentenlijst.studentnummer ,
-			'bpvdocent' : this.studentenlijst.bpvbegeleider,
-			'bpvbedrijf' : this.studentenlijst.bedrijf,
-			'praktijkopleider' : this.studentenlijst.praktijkopleider
-		};
-	}
+	// studentenlijst = {
+	// 	'studentnummer': '',
+	// 	'roepnaam': '',
+	// 	'voorvoegsel': '',
+	// 	'achternaam': '',
+	// 	'telefoon': '',
+	// 	'woonplaats': '',
+	// 	'opleiding': '',
+	// 	'email': '',
+	// 	'praktijkopleider': '',
+	// 	'bedrijf': '',
+	// 	'bpvtelefoon': '',
+	// 	'bpvbegeleider': ''
+	// };
+  //
+	// StudentnummerInput;
+	// autofill(test)
+	// {
+	// 	console.log(test);
+	// 	this.gesprek = {
+	// 		'naam' : this.studentenlijst.roepnaam + this.studentenlijst.voorvoegsel + this.studentenlijst.achternaam,
+	// 		'studentnummer' : this.studentenlijst.studentnummer ,
+	// 		'bpvdocent' : this.studentenlijst.bpvbegeleider,
+	// 		'bpvbedrijf' : this.studentenlijst.bedrijf,
+	// 		'praktijkopleider' : this.studentenlijst.praktijkopleider
+	// 	};
+	// }
 
   gesprek = {};
   logForm(id) {
